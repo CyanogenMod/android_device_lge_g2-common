5c01c614bd10501d442c0ae177e6386657c8f6ae
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-28 01:41:02 +0000

    Update gfx configs
    The hwui makefile values are less efficient than the hammerhead's, so
    use those

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -136,3 +136,14 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/lge/g2-common/releasetools
 
 COMMON_GLOBAL_CFLAGS += -DBOARD_CHARGING_CMDLINE_NAME='"androidboot.mode"' -DBOARD_CHARGING_CMDLINE_VALUE='"chargerlogo"'
 BOARD_USES_QC_TIME_SERVICES := true
+
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
+
