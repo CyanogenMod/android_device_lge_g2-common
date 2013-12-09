1c13f362204048dcc00b8136d209a17fd92b7893
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-12-09 03:34:36 +0000

    wifi: Adjust to new driver
    The driver now reads the mac address directly from NV from kernel
    space, remove the userspace config generation code.

--- hwaddrs/getmac.c
+++ hwaddrs/getmac.c
@@ -19,7 +19,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-/* Read plain address retrieved by RIL and set the Wifi MAC 
+/* Read plain address from NV and set the BT MAC
  * address accordingly */
 
 int main() {
@@ -29,29 +29,6 @@ int main() {
 	int i;
 
 	fd1 = open("/dev/block/platform/msm_sdcc.1/by-name/misc",O_RDONLY);
-	fd2 = open("/data/misc/wifi/config",O_WRONLY|O_CREAT|O_TRUNC,S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
-
-	write(fd2,"btc_mode=1\n",11);
-	write(fd2,"mpc=1\n",6);
-	write(fd2,"roam_off=0\n",11);
-	write(fd2,"roam_scan_period=10\n",20);
-	write(fd2,"roam_delta=20\n",14);
-	write(fd2,"roam_trigger=-80\n",17);
-	write(fd2,"PM=2\n",5);
-	write(fd2,"cur_etheraddr=",14);
-
-
-	for (i = 0; i<6; i++) {
-		lseek(fd1,0x3000+i,SEEK_SET);
-		lseek(fd2,0,SEEK_END);
-		read(fd1,&macbyte,1);
-		sprintf(macbuf,"%02x",macbyte);
-		write(fd2,&macbuf,2);
-		if (i!=5) write(fd2,":",1);
-	}
-
-	write(fd2,"\n",1);
-	close(fd2);
 
 	fd2 = open("/data/misc/bdaddr",O_WRONLY|O_CREAT|O_TRUNC,S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
 	for (i = 0; i<6; i++) {
