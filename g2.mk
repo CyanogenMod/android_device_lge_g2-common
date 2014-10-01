0e908571bf5cc4bddb1eaeb96e369f906e9b2c5c
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2014-10-01 12:43:39 +0200

    g2: update gps config
    Change-Id: I5bb88e04d33faaa97af9fc80f8e34e5a98fa4b73

--- g2.mk
+++ g2.mk
@@ -68,10 +68,14 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
 	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
 
+# For GPS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
 # GPS configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps.conf:system/etc/gps.conf
+    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/configs/izat.conf:system/etc/izat.conf
 
 PRODUCT_PACKAGES += \
     charger_res_images \
