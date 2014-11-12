03ee173bf9d0a646baf3dc631346f7a913d8c35e
Rashed Abdel-Tawab <rashed@linux.com>
2014-11-11 21:32:26 -0500

    g2: Add dex-preoptimization
    Change-Id: I4b05d9ea82ca1ef49c89ca286ca75879e183c27b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -35,6 +35,16 @@ TARGET_KRAIT_BIONIC_PLDSIZE := 64
 TARGET_NO_RADIOIMAGE := true
 TARGET_NO_BOOTLOADER := true
 
+# Dex-preoptimization
+ifeq ($(HOST_OS),linux)
+  ifeq ($(TARGET_BUILD_VARIANT),user)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
+WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
+
 # Kernel information
 BOARD_KERNEL_BASE     := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=g2 user_debug=31 msm_rtb.filter=0x0
