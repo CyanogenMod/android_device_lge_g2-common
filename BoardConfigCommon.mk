75b694cead740842b73e56b7e24da077e9197824
Ricardo Cerqueira <ricardo@cyngn.com>
2014-11-14 12:49:43 +0000

    Remove NFC_HAL_SUFFIX
    The new default works now
    
    Change-Id: I07b65e5034d5f246fd39f92fd097b8fcb12c7f3c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -151,8 +151,6 @@ BOARD_SEPOLICY_UNION += \
         genfs_contexts \
         te_macros
 
-BOARD_NFC_HAL_SUFFIX := g2
-
 BOARD_RIL_CLASS := ../../../device/lge/g2-common/ril/
 TARGET_RELEASETOOLS_EXTENSIONS := device/lge/g2-common/releasetools
 
