895948bfcda81ea8899c8808e3804ca5e256e880
Ricardo Cerqueira <ricardo@cyngn.com>
2014-11-16 03:32:50 +0000

    Remove old crud
    This hasn't been used in over a year
    
    Change-Id: I8aba92f5c195c3e48753e6e17642e9743257516d

--- g2.mk
+++ g2.mk
@@ -34,7 +34,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/configs/snd_soc_msm_Taiko:system/etc/snd_soc_msm/snd_soc_msm_Taiko \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml
