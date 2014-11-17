e4e3bff365bbdba5874b53340a3ecb96094754e0
myfluxi <linflux@arcor.de>
2014-11-17 12:33:00 -0500

    g2: : Add SELinux policy for healthd
    This adresses denials such as:
    avc:  denied  { read write } for  pid=144 comm="healthd" name="rtc0"
    dev="tmpfs" ino=6565 scontext=u:r:healthd:s0 tcontext=u:object_r:rtc:s0
    tclass=chr_f
    avc:  denied  { open } for  pid=143 comm="healthd" name="/" dev="tmpfs"
    ino=5405 scontext=u:r:healthd:s0 tcontext=u:object_r:device:s0 tclass=dirile
    
    Change-Id: I4854847db0e56f4189bf73bbeed992dc8a42997e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -121,6 +121,7 @@ BOARD_SEPOLICY_UNION += \
         device.te \
         domain.te \
         file.te \
+        healthd.te \
         hostapd.te \
         irsc_util.te \
         mediaserver.te \
