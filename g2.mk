c301a7311182d32302156e848661fcaeebe44a32
Seth Shelnutt <shelnutt2@gmail.com>
2015-01-06 20:52:15 -0500

    nfc: Remove hce access as it never worked and now causes linker errors for nxp devices
    Change-Id: I3d8e09baefac2b3ca65bf1a30142bc21c91627b9

--- g2.mk
+++ g2.mk
@@ -114,7 +114,6 @@ PRODUCT_COPY_FILES += \
     $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
 
 PRODUCT_PROPERTY_OVERRIDES += \
         ro.sf.lcd_density=480 \
