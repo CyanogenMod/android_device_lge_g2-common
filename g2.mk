7e8988abbfba11fdcc93ea86884a1afec550c3d2
Rashed Abdel-Tawab <rashed@linux.com>
2015-01-13 20:13:23 -0500

    fstab: Support f2fs-formatted cache and userdata
    * Add support for f2fs-formatted cache and userdata
    * Build file management tools
    
    Depends on http://review.cyanogenmod.org/86790
    
    Change-Id: I4d7c514524d9d3c469ed9ef420406cec62186961

--- g2.mk
+++ g2.mk
@@ -116,7 +116,9 @@ PRODUCT_PACKAGES += \
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
-    e2fsck
+    make_ext4fs \
+    e2fsck \
+    setup_fs
 
 PRODUCT_PACKAGES += \
     libgenlock \
