3471c2c6828e8f021ac7cf46e6bc3e989f1ff54a
Rashed Abdel-Tawab <rashed@linux.com>
2015-01-13 20:13:23 -0500

    fstab: Support f2fs-formatted cache and userdata
    -Add support for f2fs-formatted cache and userdata
    -Build file management tools
    
    Change-Id: I4d7c514524d9d3c469ed9ef420406cec62186961

--- g2.mk
+++ g2.mk
@@ -89,6 +89,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     charger_res_images
 
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    e2fsck \
+    setup_fs
+
 # Live Wallpapers
 PRODUCT_PACKAGES += \
     LiveWallpapers \
