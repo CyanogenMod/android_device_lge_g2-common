1fa5a34c9e7339e6f948fbb7b81bf46f43c5f9ef
Rashed Abdel-Tawab <rashed@linux.com>
2015-02-11 23:18:34 -0500

    g2: Refactor and cleanup
    * Come on, did no one think of ever cleaning this?
    * Why are we still copying init files when every other device builds them?
    * Fix a stray \ (messes up the rest of the product file copying)
    * What happened to the general convention on styling fstab? Fix it
    
    Change-Id: I2f35e096412275cbbd43e5d77b8b1f2faa0bd9b0

---
+++ rootdir/Android.mk
@@ -0,0 +1,37 @@
+LOCAL_PATH := $(call my-dir)
+
+# Device init files
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.g2
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab.g2
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+# Init scripts
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.g2.rc
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.g2.rc
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.g2.usb.rc
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.g2.usb.rc
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := ueventd.g2.rc
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/ueventd.g2.rc
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
