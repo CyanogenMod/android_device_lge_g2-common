c28f42df4255707f8b75222b5bbf1234c112d6ea
Rashed Abdel-Tawab <rashed@linux.com>
2015-02-11 22:55:41 -0500

    g2: releasetools: Add panel detection script
    * On 4.4.2 bootloaders and later, the bootloader automatically
      appends the panel varient to the kernel command line on boot.
      Since there are users still on 4.2.2 bootloaders, we have to
      make up for this by simply adding the varient argument to the
      kernel command line in the build.
    * LG's stupidity led them to build some models with LGD panels
      and others with JDI panels. Trying to run the kernel with the
      wrong argument results in some pretty wonky display crap, so
      detect the varient on the device and simply edit the kernel
      command line to match the varient.
    
    Change-Id: Ib5e7ab2ee2199bf5a5dddc396c731e5cecee292b

---
+++ releasetools/panel.sh
@@ -0,0 +1,28 @@
+#!/sbin/sh
+#
+# 4.4 and beyond Kernel - Panel Detection - dr87 & Rashed97
+#
+# Detect panel and swap as necessary 
+# lcd_maker_id is determined by get_panel_maker_id on the hardware and is always accurate
+# This searches directly in the boot.img and has no other requirements
+# Do not shorten the search or you may change the actual kernel source
+#
+#	LCD_RENESAS_LGD = 0,
+#	LCD_RENESAS_JDI = 1
+#
+# Some ROMs will place boot.img in /tmp instead of /tmp/loki
+# so support finding boot.img both locations
+#
+
+lcdmaker=$(grep -c "lcd_maker_id=1" /proc/cmdline)
+if [ $lcdmaker == 1 ]; then
+	echo "JDI panel detected"
+	find /tmp/loki/boot.img -type f -exec sed -i 's/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd/g' {} \;
+	find /tmp/boot.img -type f -exec sed -i 's/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd/g' {} \;
+elif [ $lcdmaker == "0" ]; then
+	echo "LGD panel detected"
+	find /tmp/loki/boot.img -type f -exec sed -i 's/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd/g' {} \;
+	find /tmp/boot.img -type f -exec sed -i 's/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd/g' {} \;
+else
+	echo "lcd_maker_id doesn't exist. Something went wrong."
+fi
