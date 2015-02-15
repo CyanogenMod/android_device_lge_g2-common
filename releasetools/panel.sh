01dc48390f031a591d77011094e1a10786fd4aee
Rashed Abdel-Tawab <rashed@linux.com>
2015-02-15 00:36:08 -0500

    g2: releasetools: Don't edit command line on 4.4.2 bootloader
    Change-Id: Id8932bfef6a947226734224a5d523987f64f9958

--- releasetools/panel.sh
+++ releasetools/panel.sh
@@ -13,16 +13,23 @@
 # Some ROMs will place boot.img in /tmp instead of /tmp/loki
 # so support finding boot.img both locations
 #
-
+blcheck=$(grep -c "mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2" /proc/cmdline)
 lcdmaker=$(grep -c "lcd_maker_id=1" /proc/cmdline)
-if [ $lcdmaker == 1 ]; then
-	echo "JDI panel detected"
-	find /tmp/loki/boot.img -type f -exec sed -i 's/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd/g' {} \;
-	find /tmp/boot.img -type f -exec sed -i 's/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd/g' {} \;
-elif [ $lcdmaker == "0" ]; then
-	echo "LGD panel detected"
-	find /tmp/loki/boot.img -type f -exec sed -i 's/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd/g' {} \;
-	find /tmp/boot.img -type f -exec sed -i 's/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd/g' {} \;
+if [ $blcheck == 1 ]; then
+	echo "Command line panel attribute detected. No need to edit boot.img"
+elif [ $blcheck == 0 ]; then
+	echo "Command line panel attribute not found. Editing boot.img"
+	if [ $lcdmaker == 1 ]; then
+		echo "JDI panel detected"
+		find /tmp/loki/boot.img -type f -exec sed -i 's/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd/g' {} \;
+		find /tmp/boot.img -type f -exec sed -i 's/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd/g' {} \;
+	elif [ $lcdmaker == "0" ]; then
+		echo "LGD panel detected"
+		find /tmp/loki/boot.img -type f -exec sed -i 's/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd/g' {} \;
+		find /tmp/boot.img -type f -exec sed -i 's/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd/g' {} \;
+	else
+		echo "lcd_maker_id doesn't exist. Something went wrong."
+	fi
 else
-	echo "lcd_maker_id doesn't exist. Something went wrong."
+	echo "cmdline doesn't exist. Something went wrong."
 fi
