12fc8e8b6e0df5cd008997b1938eb8783f851482
Rashed Abdel-Tawab <rashed@linux.com>
2015-03-01 14:10:55 -0500

    g2: Add missing symlinks to Android.mk
    Change-Id: I06163ac3f5eabc0a21131636135961f85f7f6375

--- Android.mk
+++ Android.mk
@@ -1,11 +1,78 @@
-ifneq ($(filter d800 d801 d802 l01f ls980 vs980,$(TARGET_DEVICE)),)
+#
+# Copyright 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+ifneq ($(filter g2 d800 d801 d802 d803 f320 l01f ls980 vs980,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
-$(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wcd9320; \
-        ln -sf /data/misc/audio/wcd9320_anc.bin \
-        $(TARGET_OUT_ETC)/firmware/wcd9320/wcd9320_anc.bin)
+include $(CLEAR_VARS)
+
+define vfatfilename
+$(foreach f,$(1),$(shell echo $(f) | \
+    awk 'BEGIN { FS="."; } { printf("%s.%s", substr($$1,1,8), $$2); }'))
+endef
+
+DXHDCP2_IMAGES := \
+    dxhdcp2.b00 dxhdcp2.b01 dxhdcp2.b02 dxhdcp2.b03 dxhdcp2.mdt
+
+DXHDCP2_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(DXHDCP2_IMAGES))
+$(DXHDCP2_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "dxhdcp2 firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(call vfatfilename,$(notdir $@)) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(DXHDCP2_SYMLINKS)
+
+TQS_IMAGES := \
+    tqs.b00 tqs.b01 tqs.b02 tqs.b03 tqs.mdt
+
+TQS_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(TQS_IMAGES))
+$(TQS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "tqs firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(call vfatfilename,$(notdir $@)) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(TQS_SYMLINKS)
+
+WCD9320_IMAGES := \
+    wcd9320_anc.bin wcd9320_mad_audio.bin wcd9320_mbhc.bin
+
+WCD9320_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/wcd9320/,$(WCD9320_IMAGES))
+$(WCD9320_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "wcd9320 firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	tf=$(notdir $@); if [ "$$tf" = "wcd9320_mbhc.bin" ]; then tf="mbhc.bin"; fi; ln -sf /data/misc/audio/$$tf $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(WCD9320_SYMLINKS)
+
+WIDEVINE_IMAGES := \
+    widevine.b00 widevine.b01 widevine.b02 widevine.b03 widevine.mdt
+
+WIDEVINE_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(WIDEVINE_IMAGES))
+$(WIDEVINE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "widevine firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(call vfatfilename,$(notdir $@)) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(WIDEVINE_SYMLINKS)
 
 endif
