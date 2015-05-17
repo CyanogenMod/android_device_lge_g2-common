c848991317c7af83e49caee45bba021d262012a0
Rashed Abdel-Tawab <rashed@linux.com>
2015-05-17 01:24:36 -0400

    g2: Add more GPS configs
    Change-Id: I55d69f3fdaca3543ab7fb118ff32b8126713fe97

--- g2.mk
+++ g2.mk
@@ -76,9 +76,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bluetooth/bcm4335_prepatch.hcd:system/vendor/firmware/bcm4335_prepatch.hcd
 
-# IZat configuration
+# GPS configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/izat.conf:system/etc/izat.conf
+    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/quipc.conf:system/etc/quipc.conf \
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
 
 # IRSC
 PRODUCT_COPY_FILES += \
