cadaf3cf2619815ce201b198c6a0722243a657b2
Steve Kondik <steve@cyngn.com>
2015-06-18 21:16:00 -0400

    g2: Don't need to copy FFMPEG config anymore
    Change-Id: Ib843c6398748cdc2b73ae4dfad29feb2c078226a

--- g2.mk
+++ g2.mk
@@ -36,8 +36,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 # Thermal configuration
 PRODUCT_COPY_FILES += \
