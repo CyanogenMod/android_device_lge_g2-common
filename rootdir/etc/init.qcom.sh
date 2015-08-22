918b38eff364a0351e59d978bf35c1eadaa1e713
Daniel Stuart <daniel.stuart14@gmail.com>
2015-08-22 18:36:08 -0300

    g2: Fix cdma baseband grep.
    Change-Id: I8480f17773f7bb31d62dd5de0e30f633c243a83a

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -37,7 +37,7 @@ product=`getprop ro.build.product`
 # grep the modem partition for baseband version and set it
 case "$product" in
         "ls980" | "vs980")
-        setprop gsm.version.baseband `strings /firmware/image/modem.b22 | grep "^MPSS_DI" | head -1`
+        setprop gsm.version.baseband `strings /firmware/image/modem.b18 | grep "^MPSS.DI" | head -1`
         ;;
         "d800" | "d801" | "d802" | "d803" | "f320" | "l01f")
         setprop gsm.version.baseband `strings /firmware/image/modem.b21 | grep "^M8974A-" | head -1`
