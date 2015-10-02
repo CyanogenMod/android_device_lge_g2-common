0dc1614faf89287c4950e643efbda8b0350caff3
Ethan Chen <intervigil@gmail.com>
2015-10-02 14:52:18 -0700

    g2-common: Remove unused thermal config
    Change-Id: I1456aca7677b896e9677be4b1ce48a4144ed0726

--- g2.mk
+++ g2.mk
@@ -185,8 +185,7 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine-8974.conf:system/etc/thermal-engine-8974.conf \
-    $(LOCAL_PATH)/configs/thermal-engine-default.conf:system/etc/thermal-engine-default.conf
+    $(LOCAL_PATH)/configs/thermal-engine-8974.conf:system/etc/thermal-engine-8974.conf
 
 # Touchscreen
 PRODUCT_COPY_FILES += \
