6c14a48509484ed14341f6793a4338efe1171c2a
Ethan Chen <intervigil@gmail.com>
2015-10-02 14:38:28 -0700

    g2-common: Remove unused synaptics keylayout
    Change-Id: Idb8d1d9db81ddfa253094bc68fe1bd9403b7242a

--- g2.mk
+++ g2.mk
@@ -191,7 +191,6 @@ PRODUCT_COPY_FILES += \
 # Touchscreen
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl \
     $(LOCAL_PATH)/keylayout/touch_dev.idc:system/usr/idc/touch_dev.idc
 
 # USB
