b7e4d6ca40e9c28a7aaa3376c48b07e35bd7aa48
Ethan Chen <intervigil@gmail.com>
2015-10-04 19:24:54 -0700

    g2-common: Reorganize init script makefile
    Change-Id: If21186b4e48bba15d3d77a59e191867eb9274f66

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,16 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
-# Device init files
+# Init scripts
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.g2
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.g2
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE            := fstab.g2
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/fstab.g2
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-
-# Init scripts
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.g2.rc
