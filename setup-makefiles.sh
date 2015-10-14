4911f067873d16deea5d9a4a641be166485d2a0f
Rashed Abdel-Tawab <rashed@linux.com>
2015-10-14 13:49:25 -0400

    g2: Temporarily disable qcom location apps
    Change-Id: Ida30bbee6e6a08377ba96e0d3f0642dc8e01bb10

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -154,10 +154,6 @@ PRODUCT_PACKAGES += \\
     qcrilhook
 
 PRODUCT_PACKAGES += \\
-    com.qualcomm.location \\
-    com.qualcomm.services.location
-
-PRODUCT_PACKAGES += \\
     libtime_genoff \\
     libTimeService
 
