bff1a729d75dd04f287224d00e4b4c33bcc52424
Rashed Abdel-Tawab <rashed@linux.com>
2015-10-17 21:27:24 -0400

    g2: Add LG RIL symbols via liblge
    Change-Id: I095bece6809527a2d4c67e3a5626bdbe556473b8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -114,7 +114,6 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/lge/g2-common/releasetools
 
 # RIL
 BOARD_RIL_CLASS := ../../../device/lge/g2-common/ril/
-TARGET_RELEASE_CPPFLAGS += -DNEEDS_LGE_RIL_SYMBOLS
 
 # SELinux policies
 include device/qcom/sepolicy/sepolicy.mk
