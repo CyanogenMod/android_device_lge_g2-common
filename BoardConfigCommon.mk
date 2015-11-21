dc421eabe500914893502d766dbeabee08bfc3b2
Ethan Chen <intervigil@gmail.com>
2015-11-21 04:01:26 -0600

    g2: remove custom ril class
    * GSM variants don't need this, move variant-specific hacks down
      to variant trees.
    
    Change-Id: I4f410c48df7066bf381e2e24f28c464874ffb45f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -111,9 +111,6 @@ TARGET_USERIMAGES_USE_F2FS := true
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := device/lge/g2-common/releasetools
 
-# RIL
-BOARD_RIL_CLASS := ../../../device/lge/g2-common/ril/
-
 # SELinux policies
 include device/qcom/sepolicy/sepolicy.mk
 
--- system_prop.mk
+++ system_prop.mk
@@ -82,8 +82,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     rild.libpath=/vendor/lib/libril-qc-qmi-1.so \
-    ro.ril.telephony.mqanelements=5 \
-    ro.telephony.ril_class=LgeLteRIL
+    ro.ril.telephony.mqanelements=5
 
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
