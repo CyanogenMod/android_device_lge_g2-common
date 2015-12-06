57c937ac3722c3c106ca82edb5b2d45efe6d90d2
Daniel Stuart <daniel.stuart14@gmail.com>
2015-12-06 13:03:13 -0200

    Init: Fix set_baseband script.
    Change-Id: I4fd84473d07243a15ddb2f0cd7516128579de41c

--- rootdir/etc/set_baseband.sh
+++ rootdir/etc/set_baseband.sh
@@ -1,12 +1,44 @@
 #!/system/bin/sh
+# Copyright (c) 2015, The Linux Foundation. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#     * Neither the name of The Linux Foundation nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#
 
-product=`getprop ro.build.product`
-# grep the modem partition for baseband version and set it
-case "$product" in
-        "ls980" | "vs980")
-        setprop gsm.version.baseband `strings /firmware/image/modem.b18 | grep "^MPSS.DI" | head -1`
-        ;;
-        "d800" | "d801" | "d802" | "d803" | "f320" | "l01f")
-        setprop gsm.version.baseband `strings /firmware/image/modem.b21 | grep "^M8974A-" | head -1`
-        ;;
-esac
+network=`ro.telephony.default_network`
+baseband=`getprop gsm.version.baseband`
+
+# Check if it has a CDMA or GSM radio.
+if [ "$network" == "10" ]; then
+        modem=`strings /firmware/image/modem.b18 | grep "^MPSS.DI" | head -1`
+else
+        modem=`strings /firmware/image/modem.b21 | grep "^M8974A-" | head -1`
+fi
+
+# Set baseband version by grepping it from modem partition.
+if [ "$baseband" != "$modem" ]; then
+        setprop gsm.version.baseband "$modem"
+fi
