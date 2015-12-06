99f82c2610279379f234a0e61a3324bff0149c90
Daniel Stuart <daniel.stuart14@gmail.com>
2015-12-06 13:03:13 -0200

    Init: Run set_baseband earlier, and do some cleanup.
    Change-Id: I4fd84473d07243a15ddb2f0cd7516128579de41c

--- rootdir/etc/set_baseband.sh
+++ rootdir/etc/set_baseband.sh
@@ -1,12 +1,16 @@
 #!/system/bin/sh
 
-product=`getprop ro.build.product`
-# grep the modem partition for baseband version and set it
-case "$product" in
-        "ls980" | "vs980")
-        setprop gsm.version.baseband `strings /firmware/image/modem.b18 | grep "^MPSS.DI" | head -1`
-        ;;
-        "d800" | "d801" | "d802" | "d803" | "f320" | "l01f")
-        setprop gsm.version.baseband `strings /firmware/image/modem.b21 | grep "^M8974A-" | head -1`
-        ;;
-esac
+network=`ro.telephony.default_network`
+baseband=`getprop gsm.version.baseband`
+
+# Check if it has a CDMA or GSM radio.
+if [ "$network" == "10" ]; then
+        modem=`strings /firmware/image/modem.b18 | grep "^MPSS.DI" | head -1`
+else
+        modem=`strings /firmware/image/modem.b21 | grep "^M8974A-" | head -1`
+fi
+
+# Set baseband version by grepping it from modem partition.
+if [ "$baseband" != "$modem" ]; then
+        setprop gsm.version.baseband "$modem"
+fi
