efea0a57ef579bd9c6ff606b1b7f7564ef500493
Daniel Stuart <daniel.stuart14@gmail.com>
2016-01-07 02:46:28 -0200

    init: Loop set_baseband and only exit after set it.
    Change-Id: I9cc54bb2de72e10031634dbfd4de5fcf7d6b9c8c

--- rootdir/etc/set_baseband.sh
+++ rootdir/etc/set_baseband.sh
@@ -1,12 +1,22 @@
 #!/system/bin/sh
 
 product=`getprop ro.build.product`
-# grep the modem partition for baseband version and set it
-case "$product" in
-        "ls980" | "vs980")
-        setprop gsm.version.baseband `strings /firmware/image/modem.b18 | grep "^MPSS.DI" | head -1`
-        ;;
-        "d800" | "d801" | "d802" | "d803" | "f320" | "l01f")
-        setprop gsm.version.baseband `strings /firmware/image/modem.b21 | grep "^M8974A-" | head -1`
-        ;;
-esac
+baseband=`getprop gsm.version.baseband`
+
+while true; do
+    # grep the modem partition for baseband version and set it
+    case "$product" in
+            "ls980" | "vs980")
+            setprop gsm.version.baseband `strings /firmware/image/modem.b18 | grep "^MPSS.DI" | head -1`
+            ;;
+            "d800" | "d801" | "d802" | "d803" | "f320" | "l01f")
+            setprop gsm.version.baseband `strings /firmware/image/modem.b21 | grep "^M8974A-" | head -1`
+            ;;
+    esac
+
+    sleep 8
+    # Check if baseband is set and then exit
+    if [ "$baseband" != "" ]; then
+        break
+    fi
+done
