f1be890888381696e101ee356d50e01b770adb1c
Ethan Chen <intervigil@gmail.com>
2016-02-04 11:42:51 -0800

    Revert "g2: Disable offload"
    This reverts commit 81b2e59f866d692987aacff93fa1c4e62b682cc3.
    
    Change-Id: Ie503e48fb6361f2835e47f138baac919f4ce3fe1

--- system_prop.mk
+++ system_prop.mk
@@ -43,10 +43,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.gps.agps_provider=1
 
 # Media/offload
-# TODO: Re-enable when it works
-PRODUCT_PROPERTY_OVERRIDES += \
-    audio.offload.disable=true
-
 PRODUCT_PROPERTY_OVERRIDES += \
     audio.offload.buffer.size.kb=32 \
     audio.offload.gapless.enabled=true \
