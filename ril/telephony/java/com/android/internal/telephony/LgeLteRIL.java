aa4e8fac1d72c0c147082c117140ac5021f73734
Rashed Abdel-Tawab <rashed@linux.com>
2016-02-06 13:36:52 -0500

    Revert "ril: Return static radio capability from getRadioCapability"
    Moving this to device specific RIL classes for now as it is incompatible
    with CDMA variants
    
    This reverts commit be07a7e2f670209d863e75671d3f165dc83ab1cf.
    
    CYAN-7390
    
    Change-Id: Ie4cd2c9640f1098ef3ec725121b7592dc4038b05

--- ril/telephony/java/com/android/internal/telephony/LgeLteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/LgeLteRIL.java
@@ -19,8 +19,6 @@ package com.android.internal.telephony;
 import static com.android.internal.telephony.RILConstants.*;
 
 import android.content.Context;
-import android.os.AsyncResult;
-import android.os.Message;
 import android.os.Parcel;
 
 /**
@@ -55,15 +53,5 @@ public class LgeLteRIL extends RIL implements CommandsInterface {
           failCause.vendorCause = p.readString();
         }
         return failCause;
-    }
-
-    @Override
-    public void getRadioCapability(Message response) {
-        riljLog("getRadioCapability: returning static radio capability");
-        if (response != null) {
-            Object ret = makeStaticRadioCapability();
-            AsyncResult.forMessage(response, ret, null);
-            response.sendToTarget();
-        }
     }
 }
