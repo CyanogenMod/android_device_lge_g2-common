d53d9b3ef045114354233dc49eac7acffa3ab1e9
Sultan Qasim Khan <sultanqasim@gmail.com>
2016-04-23 03:17:41 -0400

    g2-common: lights: clean up and improve code
    * Add a prefix to our log information
    * Remove unused headers, variables and constants
    * Fix code style by declaring all functions static
    * Check dev value before setting it
    
    Change-Id: I462d12fc5d81faa85607b2b3bc65c34cbd75a1a6

--- liblight/lights.c
+++ liblight/lights.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 The CyanogenMod Project
+ * Copyright (C) 2016 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,12 +14,10 @@
  * limitations under the License.
  */
 
-
-//#define LOG_NDEBUG 0
-#define LOG_TAG "lights"
+#define LOG_TAG "g2-lights"
 
 #include <cutils/log.h>
-#include <cutils/properties.h>
+
 #include <stdint.h>
 #include <string.h>
 #include <unistd.h>
@@ -41,33 +39,22 @@ static pthread_once_t g_init = PTHREAD_ONCE_INIT;
 static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
 static struct light_state_t g_notification;
 static struct light_state_t g_battery;
-static struct light_state_t g_attention;
 
-char const*const LCD_FILE
+static const char LCD_FILE
         = "/sys/class/leds/lcd-backlight/brightness";
 
-char const*const PTN_BLINK_FILE
+static const char PTN_BLINK_FILE
         = "/sys/class/lg_rgb_led/use_patterns/blink_patterns";
 
-char const*const BACKBTN_LEFT_FILE
-        = "/sys/class/leds/button-backlight1/brightness";
+/* device methods */
 
-char const*const BACKBTN_RIGHT_FILE
-        = "/sys/class/leds/button-backlight2/brightness";
-
-/**
- * device methods
- */
-
-void
-init_globals(void)
+static void init_globals(void)
 {
     // init the mutex
     pthread_mutex_init(&g_lock, NULL);
 }
 
-static int
-write_int(char const* path, int value)
+static int write_int(char const* path, int value)
 {
     int fd;
     static int already_warned = 0;
@@ -88,8 +75,7 @@ write_int(char const* path, int value)
     }
 }
 
-static int
-write_str(char const* path, char *value)
+static int write_str(char const* path, char *value)
 {
     int fd;
     static int already_warned = 0;
@@ -110,14 +96,12 @@ write_str(char const* path, char *value)
     }
 }
 
-static int
-is_lit(struct light_state_t const* state)
+static int is_lit(struct light_state_t const* state)
 {
     return state->color & 0x00ffffff;
 }
 
-static int
-rgb_to_brightness(struct light_state_t const* state)
+static int rgb_to_brightness(struct light_state_t const* state)
 {
     int color = state->color & 0x00ffffff;
     return ((77 * ((color >> 16) & 0x00ff))
@@ -125,8 +109,7 @@ rgb_to_brightness(struct light_state_t const* state)
             + (29 * (color & 0x00ff))) >> 8;
 }
 
-static int
-set_light_backlight(UNUSED struct light_device_t* dev,
+static int set_light_backlight(UNUSED struct light_device_t* dev,
         struct light_state_t const* state)
 {
     int err = 0;
@@ -137,12 +120,9 @@ set_light_backlight(UNUSED struct light_device_t* dev,
     return err;
 }
 
-static int
-set_speaker_light_locked(UNUSED struct light_device_t* dev,
+static int set_speaker_light_locked(UNUSED struct light_device_t* dev,
         struct light_state_t const* state)
 {
-
-    int len;
     int onMS, offMS;
     unsigned int colorRGB;
     char blink_pattern[PAGE_SIZE];
@@ -169,13 +149,10 @@ set_speaker_light_locked(UNUSED struct light_device_t* dev,
     return 0;
 }
 
-static void
-handle_led_prioritized_locked(struct light_device_t* dev,
+static void handle_led_prioritized_locked(struct light_device_t* dev,
         UNUSED struct light_state_t const* state)
 {
-    if (is_lit(&g_attention)) {
-        set_speaker_light_locked(dev, &g_attention);
-    } else if (is_lit(&g_notification)) {
+    if (is_lit(&g_notification)) {
         set_speaker_light_locked(dev, &g_notification);
     } else if(is_lit(&g_battery)) {
         set_speaker_light_locked(dev, &g_battery);
@@ -185,8 +162,7 @@ handle_led_prioritized_locked(struct light_device_t* dev,
 
 }
 
-static int
-set_light_battery(struct light_device_t* dev,
+static int set_light_battery(struct light_device_t* dev,
         struct light_state_t const* state)
 {
     pthread_mutex_lock(&g_lock);
@@ -206,33 +182,9 @@ static int set_light_notifications(struct light_device_t* dev,
     return 0;
 }
 
-static int
-set_light_attention(struct light_device_t* dev,
-        struct light_state_t const* state)
-{
-    int brightness = 0;
-
-    pthread_mutex_lock(&g_lock);
-    g_attention = *state;
-
-    // PowerManagerService::setAttentionLightInternal turns off the attention
-    // light by setting flashOnMS = flashOffMS = 0
-    if (g_attention.flashOnMS == 0 && g_attention.flashOffMS == 0) {
-        g_attention.color = 0;
-    }
-    brightness = rgb_to_brightness(&g_attention);
-
-    handle_led_prioritized_locked(dev, state);
-
-    /* Do the back-button lights, too */
-    pthread_mutex_unlock(&g_lock);
-    return 0;
-}
-
 
 /** Close the lights device */
-static int
-close_lights(struct light_device_t *dev)
+static int close_lights(struct light_device_t *dev)
 {
     if (dev) {
         free(dev);
@@ -243,13 +195,8 @@ close_lights(struct light_device_t *dev)
 
 /******************************************************************************/
 
-/**
- * module methods
- */
-
-/** Open a new instance of a lights device using name */
-static int
-open_lights(const struct hw_module_t* module, char const* name,
+/* Open a new instance of a lights device using name */
+static int open_lights(const struct hw_module_t* module, char const* name,
         struct hw_device_t** device)
 {
     int (*set_light)(struct light_device_t* dev,
@@ -261,14 +208,16 @@ open_lights(const struct hw_module_t* module, char const* name,
         set_light = set_light_notifications;
     else if (0 == strcmp(LIGHT_ID_BATTERY, name))
         set_light = set_light_battery;
-    else if (0 == strcmp(LIGHT_ID_ATTENTION, name))
-        set_light = set_light_attention;
     else
         return -EINVAL;
 
     pthread_once(&g_init, init_globals);
 
     struct light_device_t *dev = malloc(sizeof(struct light_device_t));
+
+    if (!dev)
+        return -ENOMEM;
+
     memset(dev, 0, sizeof(*dev));
 
     dev->common.tag = HARDWARE_DEVICE_TAG;
