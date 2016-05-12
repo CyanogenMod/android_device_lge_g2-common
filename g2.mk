d98946e80dcc275f0ccda31278cd1fecad8c9c3a
Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>
2016-05-12 20:30:15 +0200

    g2-common: Correct dalvik heap config
    dalvik.vm.heapstartsize 8m => 16m
    dalvik.vm.heapminfree 512k => 2m
    
    Change-Id: I83afad315ef4675fb3884058c6758937894b655d
    Signed-off-by: Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>

--- g2.mk
+++ g2.mk
@@ -57,7 +57,7 @@ TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
