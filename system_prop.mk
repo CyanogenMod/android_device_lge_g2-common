e6c517a658087bc6e084912faeb808e74dc1c9c2
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-09-09 08:30:44 -0500

    g2: move to source-built libril
    * Remove the shim, we dont need that shit anymore
    * Kangbang the f out of bacon's qmi stack
    * Remove the custom ril class, this is no longer necessary
    * Use the caf ril variant (version 10)
    * Rework radio properties
    * Build librmnetctl
    
    Change-Id: I7e27a455d369abd69bc1d350e178a1e012411f05

--- system_prop.mk
+++ system_prop.mk
@@ -67,20 +67,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=/vendor/lib/libqti-perfd-client.so
 
+# QMI
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.netmgrd.qos.enable=false \
+    persist.data.qmi.adb_logmask=0 \
+    persist.qcril.disable_retry=true \
+    rild.libpath=/vendor/lib/libril-qc-qmi-1.so \
+    ro.use_data_netmgrd=true
+
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.radio.custom_ecc=1
-
-PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.apm_sim_not_pwdn=1 \
-    ro.telephony.call_ring.multiple=0
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.data.qmi.adb_logmask=0 \
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    rild.libpath=/vendor/lib/libril-qc-qmi-1.so \
-    ro.ril.telephony.mqanelements=5
+    persist.telephony.oosisdc=false
 
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
