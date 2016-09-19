03c8aa6fadc8d86cd190e2396437ca680d48de62
Daniel Stuart <daniel.stuart14@gmail.com>
2016-09-18 22:46:55 -0300

    g2: audio: redo effects and props
    Kang from g3, with some changes.
    It should fix fluence, as we don't need descriptors.
    
    Change-Id: Ib3009edf8e4d3f527faa81a1a94f25f4e5142528

--- g2.mk
+++ g2.mk
@@ -63,16 +63,16 @@ $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk
 
 # Audio
 PRODUCT_PACKAGES += \
-    audio.a2dp.default \
+    audiod \
     audio.primary.msm8974 \
+    audio_policy.msm8974 \
+    audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
-    audiod \
     libaudio-resampler \
     libqcompostprocbundle \
     libqcomvisualizer \
-    libqcomvoiceprocessing \
-    libqcomvoiceprocessingdescriptors
+    libqcomvoiceprocessing
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf \
