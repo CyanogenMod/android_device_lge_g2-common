f9f6483e19bc650bda708912212c6bfe00e41815
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-10-07 10:52:13 +0800

    g2: Enable legacy HALv1 camera mediaserver hacks
    Change-Id: I4881ce7a0342b0dc9a9c50d6870f1b072b45af82

--- system_prop.mk
+++ system_prop.mk
@@ -25,7 +25,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    camera2.portability.force_api=1
+    camera2.portability.force_api=1 \
+    media.stagefright.legacyencoder=true \
+    media.stagefright.less-secure=true
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
