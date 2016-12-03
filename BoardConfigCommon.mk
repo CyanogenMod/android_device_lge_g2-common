25740a63c672c37b2aa5315dc1ca1632b2988e7c
Tyler Dunn <ireallylikeshrek@gmail.com>
2016-12-03 14:12:35 -0500

    g2-common: Cleanup cmdline
    * Requires CONFIG_USER_DEBUG, which is disabled, making this a no-op.
    
    Change-Id: I50ac48085096c40ef8799ac1fa6b21ebac1d8599
    (cherry picked from commit 5d0892a372e83464b98b1476532c709bc9c24ff9)

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -35,7 +35,7 @@ TARGET_NO_RADIOIMAGE := true
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := device/lge/g2-common/releasetools/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=g2 user_debug=31
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=g2
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x05000000 --tags_offset 0x00000100
