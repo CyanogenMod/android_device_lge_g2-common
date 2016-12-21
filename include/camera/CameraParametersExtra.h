447bcc958ab4f019845399190e17cd4e943404c0
Rashed Abdel-Tawab <rashed@linux.com>
2016-12-20 21:16:01 -0800

    g2: Add more missing camera params
    Change-Id: I485bb7532f66a6f105c3a8879881bb7887a9ee24

--- include/camera/CameraParametersExtra.h
+++ include/camera/CameraParametersExtra.h
@@ -5,6 +5,7 @@ const char CameraParameters::BEAUTY_SHOT_OFF[] = "beauty-shot"; \
 const char CameraParameters::BEAUTY_SHOT_ON[] = "beauty-shot"; \
 const char CameraParameters::BURST_SHOT_OFF[] = "burst-shot"; \
 const char CameraParameters::BURST_SHOT_ON[] = "burst-shot"; \
+const char CameraParameters::FOCUS_MODE_MANUAL_POSITION[] = "manual-position"; \
 const char CameraParameters::KEY_AUDIO_ZOOM[] = "audio-zoom"; \
 const char CameraParameters::KEY_AUDIO_ZOOM_SUPPORTED[] = "audio-zoom-supported"; \
 const char CameraParameters::KEY_BEAUTY_SHOT[] = "beauty-shot"; \
@@ -18,6 +19,7 @@ const char CameraParameters::KEY_VIDEO_WDR[] = "video-wdr"; \
 const char CameraParameters::KEY_VIDEO_WDR_SUPPORTED[] = "video-wdr-supported"; \
 const char CameraParameters::VIDEO_WDR_OFF[] = "video-wdr"; \
 const char CameraParameters::VIDEO_WDR_ON[] = "video-wdr"; \
+const char CameraParameters::WHITE_BALANCE_MANUAL_CCT[] = "white-balance-manual"; \
 const char CameraParameters::OBJECT_TRACKING_ON[] = "object-tracking"; \
 const char CameraParameters::OBJECT_TRACKING_OFF[] = "object-tracking";
 
@@ -29,6 +31,7 @@ static const char BEAUTY_SHOT_OFF[]; \
 static const char BEAUTY_SHOT_ON[]; \
 static const char BURST_SHOT_OFF[]; \
 static const char BURST_SHOT_ON[]; \
+static const char FOCUS_MODE_MANUAL_POSITION[]; \
 static const char KEY_AUDIO_ZOOM[]; \
 static const char KEY_AUDIO_ZOOM_SUPPORTED[]; \
 static const char KEY_BEAUTY_SHOT[]; \
@@ -42,6 +45,7 @@ static const char KEY_VIDEO_WDR[]; \
 static const char KEY_VIDEO_WDR_SUPPORTED[]; \
 static const char VIDEO_WDR_OFF[]; \
 static const char VIDEO_WDR_ON[]; \
+static const char WHITE_BALANCE_MANUAL_CCT[]; \
 static const char OBJECT_TRACKING_ON[]; \
 static const char OBJECT_TRACKING_OFF[];
 
